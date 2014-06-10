0455b3858cc41ffab5dfbe96eed353c37f7b83f1
damien <dalien2611@hotmail.fr>
2014-06-10 19:25:12 -0400

    initial

--- app.js
+++ app.js
@@ -2,27 +2,60 @@ var fs = require('fs')
  , gm = require('gm')
  , express = require('express');
 
+var port = (process.env.VCAP_APP_PORT || 3000);
+
 var app = module.exports = express();
 
-app.get('/image', function(req, res){
+app.get('/image', function (req, res) {
 
  console.log('resizing image to :');
- console.log(req.query );
+ console.log(req.query);
+
+ var width = 500;
+ var height = 500;
+
+ if(req.query.width != null && req.query.height !=null){
+  width = req.query.width;
+  height = req.query.height;
+ }
 
  gm('DSC03282.JPG')
-  .resize(req.query.width, req.query.height)
+  .resize(width, height)
   .noProfile()
   .toBuffer(function (err, buffer) {
-
    res.writeHead(200, {'Content-Type': 'image/jpeg' });
    res.end(buffer, 'binary');
-
-   console.log('image send !' );
+   console.log('image send !');
 
   });
 });
 
-app.listen(3000);
+app.get('/image2', function (req, res) {
+
+ console.log('resizing image to :');
+ console.log(req.query);
+
+ var width = 500;
+ var height = 500;
+
+ if(req.query.width != null && req.query.height !=null){
+  width = req.query.width;
+  height = req.query.height;
+ }
+
+ gm('image2.jpg')
+  .resize(width, height)
+  .noProfile()
+  .toBuffer(function (err, buffer) {
+   res.writeHead(200, {'Content-Type': 'image/jpeg' });
+   res.end(buffer, 'binary');
+   console.log('image send !');
+
+  });
+});
+
+
+app.listen(port);
 console.log("Go Image API : 3000");
 
 
